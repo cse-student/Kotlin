a77692e4a38c12bed5128865e664c0f6bf7a0a9c
akhilesh <akhileshseechurn@gmail.com>
2019-07-15 23:56:11 +0400

    [Inheritance and Interfaces] Overriding function declared in superclass while using its functionality

--- Inheritance and Interfaces/src/main.kt
+++ Inheritance and Interfaces/src/main.kt
@@ -8,11 +8,13 @@ fun main() {
     //region SuperClass
     var sup = SuperClass(10)
     println(sup)
+    println(sup.multiply(2))
     //endregion
 
     //region SubClass
     var sub = SubClass(15)
     println(sub)
+    println(sub.multiply(2))
     //endregion
 }
 
@@ -21,15 +23,25 @@ open class SuperClass(x: Int){
 
     //overriding toString function
     override fun toString(): String {
-        return "Superclass x = $x"
+        return "${this::class.simpleName} x = $x"
     }
     //endregion
+
+    open fun multiply(factor: Int): Int{
+        return x * factor
+    }
 }
 
 //region extending SuperClass
 class SubClass(x: Int): SuperClass(x){
     override fun toString(): String {
-        return "Subclass x = $x"
+        return "${this::class.simpleName} x = $x"
     }
+
+    //region overriding function declared in superclass while using its functionality
+    override fun multiply(factor: Int): Int {
+        return super.multiply(factor) * factor
+    }
+    //endregion
 }
 //endregion
